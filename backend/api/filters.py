766fa83fbf0f698f9f33d93d110bc758d6854631
alexeypydev <116307434+alexeypydev@users.noreply.github.com>
2023-07-06 09:26:50 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -35,10 +35,10 @@ class RecipeFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(favorite_list__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(shopping_cart__user=self.request.user)
+            return queryset.filter(shopping_list__user=self.request.user)
         return queryset
