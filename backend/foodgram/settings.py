bc35a9a6e6b1512d7d412aaafa8bcf4e2c3d1651
alexeypydev <kazus.lesha@yandex.ru>
2023-07-08 21:38:02 +0300

    Installed Debug False

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['84.201.162.226', '127.0.0.1', 'localhost']
 
